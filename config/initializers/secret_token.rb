f1877033c17cf0bf4c3b905875499e31088c76bd
sanjeevk <Sanjeevk.impinge@gmail.com>
2014-02-21 12:05:32 +0530

    inital commit + rpsec + devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todo::Application.config.secret_key_base = '73e60cea39178d9c5ade0a95a273e0134aaa9f579558fb8f89c15bd2cb4a39f09e9de12fc4af9365741fa7ca07e3ad8e6c6b44e9e360b4a050ac0c8c833c7910'
